ac26cd2416c21a0dd6082fc422c4eb9dc058db43
pswitala <p.switala@pslab.eu>
2020-02-17 22:34:44 +0100

    Added depends on ebs attachment

--- main.tf
+++ main.tf
@@ -20,4 +20,6 @@ resource "aws_volume_attachment" "pscloud-ebs-volumes-attachment" {
   device_name               = var.pscloud_ebs_volumes[count.index].device_name
   volume_id                 = aws_ebs_volume.pscloud-ebs-volumes[count.index].id
   instance_id               = var.pscloud_ec2_instance_id
+
+  depends_on = [ aws_ebs_volume.pscloud-ebs-volumes ]
 }
